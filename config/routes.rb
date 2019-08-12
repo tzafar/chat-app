063a348a298e46ce3961fdcdf263055fd8aa1a10
Toseef Zafar <mtouseef.zafar@gmail.com>
2019-08-12 16:07:58 +0100

    first controller method

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/hellos/hello', to: 'hellos#hello'
+  get 'hellos/hello', to: 'hellos#hello'
 end

78245d0fb9470f9ac255b754946eb4b007fccaad
Toseef Zafar <mtouseef.zafar@gmail.com>
2019-08-12 16:12:05 +0100

    first controller method

--- app/controllers/hello_controller.rb
+++
@@ -1,5 +0,0 @@
-class HelloController < ApplicationController
-  def hello
-    "Hello world"
-  end
-end
---
+++ app/controllers/hellos_controller.rb
@@ -0,0 +1,5 @@
+class HellosController < ApplicationController
+  def hello
+    "Hello world"
+  end
+end
